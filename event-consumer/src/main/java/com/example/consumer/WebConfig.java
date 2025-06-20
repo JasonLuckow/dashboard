84de2c9cddbccebb1e8255df0c31d18bfc27ae9f
JasonLuckow <ijasonluck@gmail.com>
2025-06-20 18:42:32 -0500

    Adding kubernetes for docker desktop deployment configuration files

--- event-consumer/src/main/java/com/example/consumer/WebConfig.java
+++ event-consumer/src/main/java/com/example/consumer/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost", "http://localhost:5173")
+            .allowedOrigins("http://localhost:30001")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true);
     }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      axios.get('http://localhost:8080/events')
+      axios.get('http://localhost:30080/events')
         .then(res => setEvents(res.data))
         .catch(err => console.error(err));
     }, 3000);
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div>
-      <h1>Live User Events</h1>
+      <h1>Live User Eventsss</h1>
       <ul>
         {events.map((e, i) => (
           <li key={i}>{e.timestamp} - User {e.userId} did {e.type}</li>
